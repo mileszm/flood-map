0f233040230a432d453934c9f9c8195700dfdd49
Miles <miles.martin@nyu.edu>
2020-02-27 18:18:31 -0500

    Added two GeoJSON layers with FEMA flood data

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,42 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlsZXNtaWxlc21pbGVzIiwiYSI6ImNrNms1NGplYzAwb3gzamtnajRkZXU3NnQifQ.nWRgr-9qGhfLQrZTI4CdFA';
 
 var initOptions = {
-  container: 'mapdiv',
-  style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-73.96, 40.72],
+  container: 'map-div',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-74, 40.725],
   zoom: 12
 }
 
 var mapObject = new mapboxgl.Map(initOptions)
 
-pizzaDirectory.forEach(
-  function(pizzaSpot) {
-    new mapboxgl.Marker()
-      .setLngLat([pizzaSpot.longitude,pizzaSpot.latitude])
-      .setPopup(
-        new mapboxgl.Popup({offset: 25})
-        .setHTML(`<p>${pizzaSpot.name} likes ${pizzaSpot.pizza_place}</p>`)
-      )
-      .addTo(mapObject)
+mapObject.addControl(new mapboxgl.NavigationControl())
+
+mapObject.on('load', function() {
+  mapObject.addSource('flood1', {
+    type: 'geojson',
+    data: './data/lvl1.geojson',
+  })
+  mapObject.addSource('flood2', {
+    type: 'geojson',
+    data: './data/lvl2.geojson',
+  })
+
+  mapObject.addLayer({
+      id: 'flood1-shading',
+      type: 'fill',
+      source: 'flood1',
+      paint: {
+        'fill-color': '#3333ff',
+        'fill-opacity': 0.5,
+      },
+  })
+  mapObject.addLayer({
+      id: 'flood2-shading',
+      type: 'fill',
+      source: 'flood2',
+      paint: {
+        'fill-color': '#0066ff',
+        'fill-opacity': 0.5,
+      },
+  })
 })
-
-
-
-
-// loadGeoJSON
-
-// var geojson = {
-//   type: 'FeatureCollection',
-//   features: [{
-//     type: 'Feature',
-//     geometry: {
-//       type: 'Point',
-//       coordinates: [-77.032, 38.913]
-//     },
-//     properties: {
-//       title: 'Mapbox',
-//       description: 'Washington, D.C.'
-//     }
-//   },
-//   {
-//     type: 'Feature',
-//     geometry: {
-//       type: 'Point',
-//       coordinates: [-122.414, 37.776]
-//     },
-//     properties: {
-//       title: 'Mapbox',
-//       description: 'San Francisco, California'
-//     }
-//   }]
-// };
-
-// add markers to map
-// geojson.features.forEach(function(marker) {
-//
-//   // create a HTML element for each feature
-//   var el = document.createElement('div');
-//   el.className = 'marker';
-//
-//   // make a marker for each feature and add to the map
-//   new mapboxgl.Marker(el)
-//     .setLngLat(marker.geometry.coordinates)
-//   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-//     .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-//   .addTo(map);
-//
-// });
